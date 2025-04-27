35966a52a0701bcac457b34ed1217a15213b3350
TomaszLizer <tomasz.lizer@gmail.com>
2025-04-27 16:32:43 +0200

    Add pixel font

---
+++ Sources/Layouts/Fonts.swift
@@ -0,0 +1,22 @@
+//
+//  Fonts.swift
+//  TomoCodes
+//
+//  Created by Tomasz Lizer on 27/04/2025.
+//
+
+import Ignite
+
+extension Font {
+    
+    /// Pixel art font: PressStart2P.
+    ///
+    /// https://fonts.google.com/specimen/Press+Start+2P
+    static var pressStart2P: Font {
+        Font(
+            name: "PressStart2P",
+            source: "/fonts/PressStart2P-Regular.ttf"
+        )
+    }
+}
+
